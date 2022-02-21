d4baa42c6a037f4259c9f789d8efdbe5959ac407
smmartin1 <smarie333m@gmail.com>
2022-02-21 13:54:43 -0500

    Added Pokemon to The Pokedex
    Added Bulbasaur, Quagsire, Latias, and Bidoof to the Array along with their height and type(s)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 // JavaScript Document
+
+//Pokedex
+let pokemonList = [
+	//Height is in inches
+	{
+		name: 'Bulbasaur',
+		height: 28,
+		types: ['grass', 'poison']
+	},
+	
+	{
+		name: 'Quagsire',
+		height: 55,
+		types: ['water', 'ground']
+	},
+	
+	{
+		name: 'Latias',
+		height: 55,
+		types: ['dragon', 'psychic']
+	},
+	
+	{
+		name: 'Bidoof',
+		height: 20,
+		types: 'normal'
+	}
+];
