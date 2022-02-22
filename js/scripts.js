474d29189222dfe7bff01007a5c027a3c8b55371
smmartin1 <smarie333m@gmail.com>
2022-02-22 13:41:58 -0500

    Added For Loop
    Added a for loop printing the array of Pokemon along with an If-Else statement about the height

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,35 @@ let pokemonList = [
 	{
 		name: 'Bulbasaur',
 		height: 28,
-		types: ['grass', 'poison']
+		types: ['Grass', 'Poison']
 	},
 	
 	{
 		name: 'Quagsire',
 		height: 55,
-		types: ['water', 'ground']
+		types: ['Water', 'Ground']
 	},
 	
 	{
 		name: 'Latias',
 		height: 55,
-		types: ['dragon', 'psychic']
+		types: ['Dragon', 'Psychic']
 	},
 	
 	{
 		name: 'Bidoof',
 		height: 20,
-		types: 'normal'
+		types: ['Normal']
 	}
 ];
+
+for(let i = 0; i < pokemonList.length; i++) {
+	document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ' inches)');
+	
+	//Threshold is subject to change
+	if (pokemonList[i].height > 40){
+		document.write(' - Wow, that\'s big!');
+	}
+	
+	document.write('<br>');
+}
