5842843b9edb731c09a05f4a60766de7c75e6e1e
smmartin1 <smarie333m@gmail.com>
2022-02-28 10:46:15 -0500

    Multiple Changes
    Added the array into an IIFE along with a getAll() and add() function.
    Changed the for loop into an forEach() loop with the output still being the same.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,51 @@
 // JavaScript Document
 
 //Pokedex
-let pokemonList = [
-	//Height is in inches
+let pokemonRepository = (function () {
+  let pokemonList = [
 	{
 		name: 'Bulbasaur',
 		height: 28,
 		types: ['Grass', 'Poison']
 	},
-	
 	{
 		name: 'Quagsire',
 		height: 55,
 		types: ['Water', 'Ground']
 	},
-	
 	{
 		name: 'Latias',
 		height: 55,
 		types: ['Dragon', 'Psychic']
 	},
-	
 	{
 		name: 'Bidoof',
 		height: 20,
 		types: ['Normal']
 	}
-];
+	];
 
-for(let i = 0; i < pokemonList.length; i++) {
-	document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ' inches)');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+	document.write(pokemon.name + ' (Height: ' + pokemon.height + ' inches)');
 	
 	//Threshold is subject to change
-	if (pokemonList[i].height > 40){
+	if (pokemon.height > 40){
 		document.write(' - Wow, that\'s big!');
 	}
 	
 	document.write('<br>');
-}
+});
